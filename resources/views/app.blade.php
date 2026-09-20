589c78c5789b51796b33ad6422e8f41f0b280459
Jaired Jawed <me@jairedjawed.com>
2026-09-20 15:20:53 +0200

    feat(branding): use cafe mark as favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,7 +9,6 @@
             html { background-color: #e7d9f8; }
         </style>
 
-        <link rel="icon" href="/favicon.ico" sizes="any">
         <link rel="icon" href="/favicon.svg" type="image/svg+xml">
         <link rel="apple-touch-icon" href="/apple-touch-icon.png">
 
